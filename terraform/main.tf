ac047550c5a0827b25af292625fbbaee45ed1301
Raman Gupta <69361319+ramangupta-rg@users.noreply.github.com>
2024-06-10 16:35:22 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name  = "hello-world"
+    image = "<your_dockerhub_username>/hello-world:latest"
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+ 
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "docker_image" {
+  description = "Docker image to use for the ECS task"
+  type        = string
+}
